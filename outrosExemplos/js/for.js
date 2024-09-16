8a88c9589b3139814be5cfb4364776aee2b0a14e
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2024-09-16 20:03:38 -0300

    Add files via upload

---
+++ outrosExemplos/js/for.js
@@ -0,0 +1,7 @@
+for (let hora = 0; hora < 24; hora++){
+    for(let minuto = 0; minuto < 60; minuto++){
+        for(let segundo = 0; segundo <60; segundo++){
+            console.log(hora + " : " + minuto + " : " + segundo)
+        }
+    }
+}
---
+++ outrosExemplos/js/repeticao.js
@@ -0,0 +1,11 @@
+let contadora = 1 
+
+while (contadora<=200){
+    if(contadora %2 ==0){
+        console.log("O número é: "+contadora+ " ele é par")
+    }else{
+        console.log("O número é: "+contadora+ " ele é impar")
+    }
+
+    contadora++
+}
